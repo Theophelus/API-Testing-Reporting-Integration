25f0dcc12bf4c680a36dc4e0965cb2728aa69546
Theophelus <aneletom10@gmail.com>
2024-10-19 17:55:37 +0200

    Overriding ItestListener abstract methods and getting current ExtentReport instance

--- src/test/java/org.anele/listeners/ExtentTestNGListener.java
+++ src/test/java/org.anele/listeners/ExtentTestNGListener.java
@@ -1,6 +1,21 @@
 package org.anele.listeners;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 public class ExtentTestNGListener implements ITestListener {
+
+    protected static ExtentReports extent;
+
+    @Override
+    public void onStart(ITestContext context) {
+        //get instance of current Extent report
+        extent = ExtentManager.getReport();
+    }
+
+
 }
