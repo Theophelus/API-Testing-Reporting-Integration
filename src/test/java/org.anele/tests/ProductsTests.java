de5e7e0d4d1ae31777efd3737aa0e20ade88249c
Theophelus <aneletom10@gmail.com>
2024-10-19 21:14:37 +0200

    Cleaning ProductsTests.java class

--- src/test/java/org.anele/tests/ProductsTests.java
+++ src/test/java/org.anele/tests/ProductsTests.java
@@ -6,31 +6,24 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.restassured.response.Response;
 import org.anele.base.BaseTest;
+import org.anele.listeners.ExtentTestNGListener;
 import org.anele.model.Product;
 import org.anele.model.Products;
 import org.anele.utils.PropertyFileManager;
 import org.apache.http.HttpStatus;
 import org.testng.Assert;
 
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Ignore;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-import java.io.File;
-import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
+@Listeners(ExtentTestNGListener.class)
 public class ProductsTests {
 
     protected BaseTest baseCore;
     protected static PropertyFileManager PROPERTY_FILE_MANAGER;
-    //define ExtentReports object to be used across test suites
-    protected ExtentReports extent;
 
     public ProductsTests() {
         this.baseCore = new BaseTest();
@@ -46,26 +39,22 @@ public class ProductsTests {
     @AfterSuite
     public void tearDown() {
         //this will ensure the report is created
-        extent.flush();
+//        extent.flush();
     }
 
     //create a method to build the response
     @Test
-    public void getAllProducts() {
-        //test object to log out details about this test
-        ExtentTest test = extent.createTest("GET_all_product", "This test is to get all products in the api");
+    public void testGETAllProducts() {
         //get the get all products operation
         Response httpResponse = baseCore.getOperation();
         //check status code of the response, if not 200. fail the test
         if (httpResponse.statusCode() != HttpStatus.SC_OK)
-            throw new RuntimeException("Request failed: " + httpResponse.statusCode());
+            Assert.assertEquals(httpResponse.statusCode(), HttpStatus.SC_OK, "Request failed: " + httpResponse.statusCode());
 
         int actual_results = httpResponse.jsonPath().getList("products").size();
         int expected_results = 30;
         Assert.assertEquals(actual_results, expected_results, "provide values do not match");
 
-        //log basic test info
-        test.log(Status.INFO, "GET request is executed successfully");
     }
 
     @Test
