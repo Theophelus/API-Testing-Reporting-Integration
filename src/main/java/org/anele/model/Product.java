cd3e21bcf5b28e4ed5adf5e97f73054d13dc0f22
Theophelus <aneletom10@gmail.com>
2024-11-02 17:38:00 +0200

    Refactored Product.java class

--- src/main/java/org/anele/model/Product.java
+++ src/main/java/org/anele/model/Product.java
@@ -28,4 +28,92 @@ public class Product {
     public Meta meta;
     public ArrayList<String> images;
     public String thumbnail;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public Dimensions getDimensions() {
+        return dimensions;
+    }
+
+    public String getWarrantyInformation() {
+        return warrantyInformation;
+    }
+
+    public String getShippingInformation() {
+        return shippingInformation;
+    }
+
+    public String getAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public ArrayList<Review> getReviews() {
+        return reviews;
+    }
+
+    public String getReturnPolicy() {
+        return returnPolicy;
+    }
+
+    public int getMinimumOrderQuantity() {
+        return minimumOrderQuantity;
+    }
+
+    public Meta getMeta() {
+        return meta;
+    }
+
+    public ArrayList<String> getImages() {
+        return images;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
 }
